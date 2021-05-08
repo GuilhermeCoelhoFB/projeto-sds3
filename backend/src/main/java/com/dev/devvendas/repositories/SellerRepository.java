6c6de72590afe4a85992185c7d7fd79aa762293b
Guilherme Fernandes <guilherme.coelhofb@gmail.com>
2021-05-07 22:32:37 -0300

    Layers

---
+++ backend/src/main/java/com/dev/devvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.dev.devvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.devvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
